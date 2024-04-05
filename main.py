257dd818392a71b874952c25552a1bc838ab9129
Samin Haque <saminhaque213@gmail.com>
2024-04-06 04:59:29 +0600

    fixed login related bug

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """
 i) only a student can check his grade
 ii) make another role called teacher who will be able to check or update their grades.
-iii) check if the user is a student or a teacher
+iii) check if the user is a student or a teacher. DONE
 iv) allow marks entry only if user is a teacher.
 """
-registered_users = ['lara', 'bruce']  # put only usernames here
-passwords_arr = ['raider', "Batcave28"]  # map user passwords with registered_users
+registered_users = ['lara', 'clerk', 'joy', 'jim', 'sumbul', 'tim', 'bruce', 'victor']  # put only usernames here
+passwords_arr = ["raider", "lane", "enjoy", "pam", "harem", "code", "Batcave28", "vic"]  # map user passwords with registered_users
+user_role = ['s', 's', 's', 's', 's', 's', 't', 't']
+marks = [85, 80, 95, 75, 39, 66]
+
 
 username = input("Enter Username: ")
 password = input('Enter Password: ')
@@ -23,9 +26,10 @@ def len_counter(user) -> int:
 username_length = len_counter(username)
 
 
-def valid_username(user: str, user_length, passwrd: str, registered_arr: list) -> int:
+def valid_username(user: str, user_length, registered_arr: list) -> int:
     elem_num = 0
     for i in range(len(registered_arr)):
+        count = 1
         for j in range(user_length - 1):
             if user_length != len_counter(registered_arr[i]):
                 break
@@ -36,7 +40,12 @@ def valid_username(user: str, user_length, passwrd: str, registered_arr: list) -
             elif user_length > len_counter(registered_arr[i]):
                 break
             else:
-                elem_num = i + 1
+                count += 1
+        # print(count)
+        if count == user_length:
+            elem_num = i + 1
+            return elem_num
+
     return elem_num
     # store the returned statements in a variable
 
@@ -68,17 +77,27 @@ def load_pass(user_occurrence, passwrd_arr) -> str or int:
 
 
 # the valid_username had to be written in a redundant form; to replicate the structure of procedural programming of 8086
-# valid_user = "Invalid username" if not valid_username(username, username_length, '123', registered_users) else "WElCOME"
+# valid_user="Invalid username" if not valid_username(username, username_length, '123', registered_users) else "WElCOME"
 
 
-valid_user = valid_username(username, username_length, '123', registered_users)
+valid_user = valid_username(username, username_length, registered_users)
 # print(valid_user)
 
 
 loaded_password = load_pass(valid_user, passwords_arr)
 # print(loaded_password)
 print(authenticated := authenticate(valid_user, loaded_password, password))
+print(user_role[valid_user - 1])
 
+
+user_type = user_role[valid_user - 1]
+if user_type == 't':
+
+    print("Teacher")
+else:
+    print('Student')
 
 # places where functions were invoked; in assembly make sure to store the appropriate values within a variable to
-# replicate the function calls implemented in this program. (read all the comments within each functions)
+# replicate the function calls implemented in this program. (read all the comments within each function)
+
+
